f02ee3c7f7186aa26cf875bff092701bdaecf66f
wycodes <17835423002@163.com>
2019-03-11 20:53:23 +0800

    Hello GitHub! 新增实验

--- src/com/gitdemo/GitTest.java
+++ src/com/gitdemo/GitTest.java
@@ -1,7 +1,16 @@
 package com.gitdemo;
 
+import java.util.Date;
+
 public class GitTest {
+    /**
+     * made by IDEA
+     * author:wy
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello GitHub!");
+        Date date = new Date();
+        System.out.println(date.getTime());
     }
 }
